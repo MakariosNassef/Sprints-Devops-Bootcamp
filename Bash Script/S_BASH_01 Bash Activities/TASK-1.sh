71923e0d6bd10bea51bf066a6245cac185a41f5b
MakariosNassef <makarios059@gmail.com>
2022-11-25 13:02:54 +0200

    Update TASK-1.sh

--- Bash Script/S_BASH_01 Bash Activities/TASK-1.sh
+++ Bash Script/S_BASH_01 Bash Activities/TASK-1.sh
@@ -1,4 +1,4 @@
-/usr/bin/env bash
+#/usr/bin/env bash
 
 echo "          _____                    _____                    _____          
          /\    \                  /\    \                  /\    \         
