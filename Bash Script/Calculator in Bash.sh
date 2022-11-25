4840ba9121186a090a49924d75710e2cf8401419
MakariosNassef <makarios059@gmail.com>
2022-11-25 15:41:32 +0200

    Update Calculator in Bash.sh

--- Bash Script/Calculator in Bash.sh
+++ Bash Script/Calculator in Bash.sh
@@ -5,7 +5,6 @@ echo "Enter Two numbers : "
 read a
 read b
 
-# Input type of operation
 echo "Enter Choice :"
 echo "1. Addition"
 echo "2. Subtraction"
@@ -13,8 +12,7 @@ echo "3. Multiplication"
 echo "4. Division"
 read ch
 
-# Switch Case to perform
-# calculator operations
+
 case $ch in
 1)res=`echo $a + $b | bc`
 ;;
